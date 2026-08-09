b9d172cab619bec5faaf4aaf8328373c5f6126b1
wingc34 <learnarr222@gmail.com>
2026-08-09 16:27:20 +0900

    feat: add webhook handler

---
+++ internal/handler/webhook_handler.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/wingc34/mini-commerce-api/internal/service"
+	"github.com/wingc34/mini-commerce-api/pkg/response"
+)
+
+type WebhookHandler struct {
+	service service.PaymentService
+}
+
+func NewWebhookHandler(service service.PaymentService) *WebhookHandler {
+	return &WebhookHandler{service: service}
+}
+
+func (h *WebhookHandler) HandleStripeWebhook(c *gin.Context) {
+	payload, err := io.ReadAll(c.Request.Body)
+
+	if err != nil {
+		response.Error(c, http.StatusBadRequest, "failed to read request body")
+		return
+	}
+
+	sigHeader := c.GetHeader("Stripe-Signature")
+
+	err = h.service.HandleWebhook(payload, sigHeader)
+	if err != nil {
+		response.Error(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"received": true})
+}
